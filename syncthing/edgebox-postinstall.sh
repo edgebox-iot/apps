cf28ee17603eb057987c88b0a258d5a8f5f70e9f
Paulo Truta <pinheirotruta5@gmail.com>
2024-12-02 13:55:23 +0100

    syncthing: Added vscode/tasks file and tweaked postinstall

--- syncthing/edgebox-postinstall.sh
+++ syncthing/edgebox-postinstall.sh
@@ -1,5 +1,3 @@
 # mkdir -p ./appdata/syncthing-ws
 echo "📦 syncthing postinstall script"
 echo "- Running in $(pwd)"
-echo "- Copying files to ./appdata/syncthing-ws"
-cp ./src/install/* ./appdata/syncthing-ws/
